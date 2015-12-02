66ae68920918f9e19297168ee389e5bcc4e78a69
islejeon <isle.jeon@gmail.com>
2015-12-02 15:55:00 +0900

    Are YOU THERE??

---
+++ practice/src/main/java/org/isle/dao/MemberMapper.java
@@ -0,0 +1,19 @@
+package org.isle.dao;
+
+import org.isle.domain.MemberVO;
+
+public interface MemberMapper extends CRUDMapper<MemberVO, String> {
+	
+	
+
+//	public void create(MemberVO vo) throws Exception;	
+//	
+//	
+//	public MemberVO read(String userid ) throws Exception;
+//	
+//	public void delete(String userid) throws Exception;	
+//	
+//	public void update(MemberVO vo) throws Exception;	
+	
+	
+}
