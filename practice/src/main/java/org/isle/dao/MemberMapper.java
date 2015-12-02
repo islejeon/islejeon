deab73c8e5cb0ea4eed77049e82597016aed9af8
najaeng <najaeng@naver.com>
2015-12-02 16:00:53 +0900

    Update MemberMapper.java
    수정내용
    
    수정이유

--- practice/src/main/java/org/isle/dao/MemberMapper.java
+++ practice/src/main/java/org/isle/dao/MemberMapper.java
@@ -7,13 +7,12 @@ public interface MemberMapper extends CRUDMapper<MemberVO, String> {
 	
 
 //	public void create(MemberVO vo) throws Exception;	
-//	
-//	
+//		
 //	public MemberVO read(String userid ) throws Exception;
 //	
 //	public void delete(String userid) throws Exception;	
 //	
 //	public void update(MemberVO vo) throws Exception;	
-	
+//수정이 되는감????
 	
 }
